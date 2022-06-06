b68b4a1579df19348975ae4a1ae91b681e53ed8e
Moupiya-M67 <1905328@kiit.ac.in>
2022-06-07 01:08:37 +0530

    Add SQLForm and its functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar title="AtlanSQL" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div class="container my-3">
+        <Home heading="Enter your SQL query" showAlert={showAlert} mode={mode} />
+      </div>
+
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+
+export default function Home(props) {
+    const runQuery = ()=>{
+       // console.log("Uppercase was clicked :  "+text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Your query running!","success");
+    }
+    
+     const handleClearClick = ()=>{
+        //clear the text
+         let newText = '';
+         setText(newText);
+         props.showAlert("Text cleared!","success");
+     }
+     const handleCopyClick = ()=>{
+        //copy the text
+        //  let newText = document.getElementById('myBox');
+        //  newText.select();
+        //  navigator.clipboard.writeText(newText.value);
+        navigator.clipboard.writeText(text);
+         props.showAlert("Text copied!","success");
+     }
+     
+    const handleOnChange = (event)=>{
+       // console.log("On change");
+        setText(event.target.value);
+    }
+    const [text, setText] = useState('');
+    return (
+        <>
+        <div className="container my-3"  style={{color: props.mode === 'dark'? 'white':'#042743'}} >
+            <h2>{props.heading}</h2>
+            <div className="mb-3">
+            <textarea className="form-control my-3" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'? 'grey':'white', color: props.mode ==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            </div>
+            <button disabled={text.length===0} className="btn btn-dark" onClick={runQuery} >Run Query</button>
+            <button disabled={text.length===0} className="btn btn-dark mx-2 my-1" onClick={handleClearClick} >Clear</button>
+            <button disabled={text.length===0} className="btn btn-dark mx-2 my-1" onClick={handleCopyClick} >Copy</button>
+        </div>
+        <div className="container my-4" style={{color: props.mode === 'dark'? 'white':'#042743'}}>
+            <h2>Your Result</h2>
+            
+            <p>{text.length >0 ? text : "No result yet"}</p>
+        </div>
+        </>
+    )
+}
